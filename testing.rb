fed404ad8209f0061051635f7493e43b8904e708
KediZhang <31493095+KediZhang@users.noreply.github.com>
2017-09-21 23:14:32 -0400

    commit

---
+++ testing.rb
@@ -0,0 +1,13 @@
+body = gets.chomp
+
+fetch = "m"
+
+day = ["mon","tue","wed","thu","fri","sat","sun"]
+
+day.each do |da|
+  if /da/ =~ body 
+  puts day.detect { |gem| /^r/ =~ gem }
+elsif 
+  
+end 
+
