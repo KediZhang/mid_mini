3fdb4d010cc5ad2441be66ca502cb11ff4f01eb0
KediZhang <31493095+KediZhang@users.noreply.github.com>
2017-09-21 23:34:14 -0400

    comi

--- testing.rb
+++ testing.rb
@@ -21,6 +21,21 @@ end
 # I have simplified the status into two catergory "busy" and "empty". Time has also been simplified into only two period: day and night.
 # Of course this data pool can be expended by dividing it into more catergories.
 
+DAY = ["mon","tue","wed","thu","fri","sat","sun"]
+
+
+
+#incoming_text_day?
+def incoming_text_day? body
+  DAY.each do |word|
+    if body.include?(word)
+      return true
+    end
+  end
+  return false
+end
+
+
 
 get "/" do
 	404
@@ -40,14 +55,45 @@ get "/sms/incoming" do
   body = body.downcase.strip
   
   if session["counter"] == 1
-    message = "Thanks for your first message. From #{sender} saying #{body}."
-    message = " I am a bot works in Tamordnual Laundromat. My name is LaundromatGo🤖 and I'd like to help you check whether it's busy or empty in our laundromat in the time period you specify"
-    message = " Message me a day in a week like 'Mon' to begin your search"
+    message = "Thanks for your first message. From #{sender} saying #{body}.I am a bot called LaundromatGo🤖 working in Tamordnual Laundromat. I'd like to help you check whether it's busy or empty in our laundromat in the time period you specify. Message me a day in a week like 'Mon' to begin your search"
     session["counter"] += 1
   else
     message = "Thank you #{sender} for reaching LaundromatGo🤖 again. Message me to begin your search"
   end
     
+  # ask for day
+  while   session["count"] == 1
+    if incoming_text_day? body
+      if /mon/ =~ body
+       fetch1 = "m"
+       text_day = "Monday"
+      elsif /tue/ =~ body
+       fetch1 = "t"
+       text_day = "Tuesday"
+      elsif /wed/ =~ body
+       fetch1 = "w"
+       text_day = "Wednesday"
+      elsif /thu/ =~ body
+       fetch1 = "r"
+       text_day = "Thursday"
+      elsif /fri/ =~ body
+       fetch1 = "f"
+       text_day = "Friday"
+      elsif /sat/ =~ body
+       fetch1 = "s"
+       text_day = "Saturday"
+      else
+       fetch1 = "u"
+       text_day = "Sunday"
+      end
+       message = "Great! Then, what time do you want to come on that day, please type intergals from 0 to 23 only"
+       session["count"] += 1
+       break
+    else
+     message = "I didn't catch that. please include the day 'Mon, Monday, etc.' in your question"
+    end
+  end 
+  
   
   twiml = Twilio::TwiML::MessagingResponse.new do |r|
     r.message do |m|
