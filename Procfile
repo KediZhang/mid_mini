ac01a8d6f6be71d02b2fb73c41ddb6a36e47e017
KediZhang <31493095+KediZhang@users.noreply.github.com>
2017-09-22 02:40:42 -0400

    aaa

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bundle exec ruby simple.rb -p $PORT
+web: bundle exec ruby simple_app.rb -p $PORT
--- simple_app.rb
+++ simple_app.rb
@@ -1,4 +1,4 @@
-# "LaundromatGo🤖" is a SMS bot for a laundromat, customers can check the whether the laundromat is crowded in the piece the time they specify. 
+# "LaundromatGo🤖" is a SMS bot for a laundromat, customers can check the whether the laundromat is crowded in the piece the day they specify. 
 # Link: https://evening-escarpment-93924.herokuapp.com/
 # Number: (412) 459 7528
 
@@ -18,17 +18,6 @@ configure :development do
 end
 
 
-# I have simplified the status into two catergory "busy" and "empty". Time has also been simplified into only two period: day and night.
-# Of course this data pool can be expended by dividing it into more catergories.
-
-busy = "m2t2w2r2f1f2s2u1u2"
-
-fetch1 = "m"
-fetch2 = "1"
-fecth = "m1"
-text_day = nil
-counter2 = 1
-
 get "/" do
 	404
 end
@@ -45,73 +34,47 @@ get "/sms/incoming" do
   body = params[:Body] || ""
   body = body.downcase.strip
   
+  
   if session["counter"] == 1
-    message = "Thanks for your first message. From #{sender} saying #{body}. I am a bot called LaundromatGo🤖 and working in Tamordnual Laundromat. I'd like to help you check whether it's busy or empty in our laundromat in the time period you specify. Message me a day in a week like 'Monday' to begin your search"
+    message = "Hi, I'm LaundromatGo🤖 and I can help you to check whether it will be crowded in our laundromat. Type the day you want to check like this 'Mon' "
     session["counter"] += 1
+    if body == "mon" 
+      message = "Great! There won't be too many people in our laundromat in Monday"
+    elsif body == "tue"
+      message = "Great! There won't be too many people in our laundromat in Tuesday"
+    elsif body == "wed"
+      message = "Great! There won't be too many people in our laundromat in Wednesday"
+    elsif body = "thu"
+      message = "Great! There won't be too many people in our laundromat in Thursday"
+    elsif body = "fri"
+      message = "Great! There won't be too many people in our laundromat in Friday"
+    elsif body = "sat"      
+      message = "Saturday will be very crowded and busy, you'd better choose another time."
+    elsif body = "sat"      
+      message = "Saturday will be very crowded and busy, you'd better choose another time."
+    else
+      message = "I didn't understand that. You can say mon, tue, wed, etc."
+    end
   else
-    message = "Thank you #{sender} for reaching LaundromatGo🤖 again. Message me to begin your search"
-  end
- 
- 
-  if body.include?('mon')
-     fetch1 = "m"
-     text_day = "Monday"
-     message = "You said #{text_day}. Then, what time do you want to come on that day? Our business hour is 9am to 10pm, so please consider type intergals from 9 to 21"
-   
-   elsif body.include?('tue')
-     fetch1 = "t"
-     text_day = "Tuesday"
-     message = "You said #{text_day}. Then, what time do you want to come on that day? Our business hour is 9am to 10pm, so please consider type intergals from 9 to 21"
-     
-   elsif body.include?('wed')
-     fetch1 = "w"
-     text_day = "Wednesday"
-     message = "You said #{text_day}. Then, what time do you want to come on that day? Our business hour is 9am to 10pm, so please consider type intergals from 9 to 21"
-     
-   elsif body.include?('thu')
-     fetch1 = "r"
-     text_day = "Thursday"
-     message = "You said #{text_day}. Then, what time do you want to come on that day? Our business hour is 9am to 10pm, so please consider type intergals from 9 to 21"
-     
-   elsif body.include?('fri')     
-     fetch1 = "f"
-     text_day = "Friday"
-     message = "You said #{text_day}. Then, what time do you want to come on that day? Our business hour is 9am to 10pm, so please consider type intergals from 9 to 21"
-     
-   elsif body.include?('sat')
-     fetch1 = "s"
-     text_day = "Saturday"
-     message = "You said #{text_day}. Then, what time do you want to come on that day? Our business hour is 9am to 10pm, so please consider type intergals from 9 to 21"
-     
-   elsif body.include?('sun')
-     fetch1 = "n"
-     text_day = "Sunday" 
-     message = "You said #{text_day}. Then, what time do you want to come on that day? Our business hour is 9am to 10pm, so please consider type intergals from 9 to 21"
-     
-   else
-     message = "I didn't understand that. You can say Mon, Monday, Tue, etc."  
-  end
-
- 
-  if body.to_i < 18
-     fetch2 = "1"
+    message = "Welcome back, please tell me what day do you want to come to our laundromat like this 'Mon'"
+    if body == "mon" 
+      message = "Great! There won't be too many people in our laundromat in Monday"
+    elsif body == "tue"
+      message = "Great! There won't be too many people in our laundromat in Tuesday"
+    elsif body == "wed"
+      message = "Great! There won't be too many people in our laundromat in Wednesday"
+    elsif body = "thu"
+      message = "Great! There won't be too many people in our laundromat in Thursday"
+    elsif body = "fri"
+      message = "Great! There won't be too many people in our laundromat in Friday"
+    elsif body = "sat"      
+      message = "Saturday will be very crowded and busy, you'd better choose another time."
+    elsif body = "sat"      
+      message = "Saturday will be very crowded and busy, you'd better choose another time."
     else
-     fetch2 = "2"
+      message = "I didn't understand that. You can say mon, tue, wed, etc."
+    end
   end
-    
-    fetch = fetch1 + fetch2
-  
-  if busy.include? fetch
-     message = "#{body} o'clock on #{text_day} will be very crowded and busy, you'd better choose another time."
-    else
-     message = "Great! There won't be too many people in our laundromat in #{body} o'clock on #{text_day}"
-  end
-    
-    
- 
-  
- 
- 
  
  
     
