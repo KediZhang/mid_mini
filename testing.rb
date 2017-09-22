c3ccce3668fda5ff529a490445969f6433d6d6b5
KediZhang <31493095+KediZhang@users.noreply.github.com>
2017-09-22 01:15:55 -0400

    q

--- testing.rb
+++ testing.rb
@@ -9,58 +9,69 @@ require 'twilio-ruby'
 
 enable :sessions
 
+@client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
+
+
+configure :development do
+  require 'dotenv'
+  Dotenv.load
+end
+
 
 # I have simplified the status into two catergory "busy" and "empty". Time has also been simplified into only two period: day and night.
 # Of course this data pool can be expended by dividing it into more catergories.
 
-DAY = ["mon","tue","wed","thu","fri","sat","sun"]
+busy = "m2t2w2r2f1f2s2u1u2"
 
-
-#incoming_text_day?
-def incoming_text_day? body
-  DAY.each do |x|
-    if body.include?('x')
-      return true
-    end
-  end
-  return false
-end
-
-
+session['fetch1'] = "m"
+session['fetch2'] = "1"
+session['fecth'] = "m1"
+session['text_day'] = "Monday"
 
 get "/" do
 	404
 end
 
 
-get "/sms/incoming/:body" do
+
+get "/sms/incoming" do
+  session["last_intent"] ||= nil
   
-  session["count"] ||= 1
-    
-  body = params[:body] || ""
+  session["counter"] ||= 1
+  session["counter2"] ||= 1
+  
+  count = session["counter"]  
+  
+  sender = params[:From] || ""
+  body = params[:Body] || ""
   body = body.downcase.strip
-  # ask for day
-  while   session["count"] == 1
-    if incoming_text_day? body
-       "Great! Then, what time do you want to come on that day, please type intergals from 0 to 23 only"
-       session["count"] += 1
-       break
-    else
-     "I didn't catch that. please include the day 'Mon, Monday, etc.' in your question"
-    end
-  end 
   
+  if session["counter"] == 1
+    message = "Thanks for your first message. From #{sender} saying #{body}. I am a bot called LaundromatGo🤖 and working in Tamordnual Laundromat. I'd like to help you check whether it's busy or empty in our laundromat in the time period you specify. Message me a day in a week like 'Monday' to begin your search"
+    session["counter"] += 1
+  else
+    message = "Thank you #{sender} for reaching LaundromatGo🤖 again. Message me to begin your search"
+  end
+ 
+ 
+ 
+ 
+ 
+ 
+    
   
-  #twiml = Twilio::TwiML::MessagingResponse.new do |r|
-   # r.message do |m|
-    #  m.body( message )
-     
-    #end 
-    #end
+  twiml = Twilio::TwiML::MessagingResponse.new do |r|
+    r.message do |m|
+      m.body( message )
+      #unless media.nil?
+       # m.media( media )
+      #end
+    end 
+  end
 
   
- # content_type 'text/xml'
- #twiml.to_s
+  content_type 'text/xml'
+  twiml.to_s
   
   
 end
